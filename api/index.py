921b8f33f62022984a6e6c8f198c04b8ad1c7583
TX15\Joel <joel-digitals@web.de>
2025-05-08 19:18:58 +0200

    1.0.0.0 hf3

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 
 # Der Pfad zu den Django-Einstellungen (ersetze 'deinprojektname' mit dem tatsächlichen Namen deines Projekts)
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deinprojektname.settings")  # Ersetze deinprojektname mit deinem tatsächlichen Projektnamen
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "joel_digitals.settings")  # Ersetze deinprojektname mit deinem tatsächlichen Projektnamen
 
 # ASGI-Anwendung von Django holen
 app = get_asgi_application()
