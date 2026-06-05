3667e98ed7646c8a5643d615eb5b4d672d69af31
Joel <joel-digitals@web.de>
2026-06-05 18:23:40 +0200

    6.1.0.0 hFx1.1

--- joel_digitals/settings.py
+++ joel_digitals/settings.py
@@ -180,7 +180,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CRON_SECRET = "jds-e-mails'
+CRON_SECRET = "jds-e-mails"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
