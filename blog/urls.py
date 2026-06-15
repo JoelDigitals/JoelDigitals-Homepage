6890b516e231f45c0bc3140ab1dc1e321fba6c21
Joel <joel-digitals@web.de>
2026-06-15 12:41:50 +0200

    6.3.0.1

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path("", views.blog_list, name="blog_list"),
     path("rss/", LatestPostsFeed(), name="blog_rss"),
     path("atom/", AtomLatestPostsFeed(), name="blog_atom"),
-    path("<slug:slug>/", views.blog_detail, name="blog_detail"),
-    path("<int:pk>/", views.blog_detail_by_pk, name="blog_detail_pk"),
     path("admin-blog/", views.admin_blog, name="admin_blog"),
     path("create/", views.blog_create, name="create"),
     path("edit/<int:pk>/", views.blog_edit, name="edit"),
@@ -16,4 +14,6 @@ urlpatterns = [
     path("categories/create/", views.create_category, name="create_category"),
     path("categories/<int:pk>/edit/", views.edit_category, name="edit_category"),
     path("categories/<int:pk>/delete/", views.delete_category, name="delete_category"),
+    path("<slug:slug>/", views.blog_detail, name="blog_detail"),
+    path("<int:pk>/", views.blog_detail_by_pk, name="blog_detail_pk"),
 ]
